aa0c6a1e9a4ad43a0564fd7ad34c58901468fef6
0xhatsume <0xhatsume@gmail.com>
2023-11-12 03:11:48 +0300

    :sparkles: Add navbar with wallet button

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,31 @@ function App() {
 
   return (
     <>
+    <header className="flex z-50 w-full bg-transparent text-sm py-4 border">
+    <nav
+        className="w-full mx-auto px-3 flex items-center justify-between"
+        aria-label="Global"
+      >
+
+
+        
+          <button
+                type="button"
+                className="
+                ml-auto mr-1
+                py-3 px-4 inline-flex items-center gap-x-2 
+                text-sm font-semibold rounded-lg text-white
+                border border-transparent
+                bg-blue-600 hover:bg-blue-900 disabled:opacity-50 disabled:pointer-events-none"
+                data-hs-overlay="#hs-vertically-centered-modal"
+                onClick={() => {}}
+              >
+                Connect Wallet
+            </button>
+
+
+      </nav>
+    </header>
       <TicTacToe />;
     </>
   )
